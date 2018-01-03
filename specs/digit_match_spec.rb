838b6a849538944b4e1c25e463a045d78dc1014c
shrutivanw <shruti.dalvi@gmail.com>
2018-01-03 12:44:45 -0800

    Digit Match test update
    Adding test for nil

--- specs/digit_match_spec.rb
+++ specs/digit_match_spec.rb
@@ -3,27 +3,37 @@ require 'minitest/reporters'
 require_relative '../lib/digit_match'
 
 describe "digit match" do
-  describe "test 1" do
-    it "4 digits match" do
-      digit_match(1072503891, 62530841).must_equal 4
+  describe "basic tests" do
+    describe "test 1" do
+      it "4 digits match" do
+        digit_match(1072503891, 62530841).must_equal 4
+      end
+    end
+
+    describe "test 2" do
+      it "10 digits match" do
+        digit_match(3862947593, 3862947593).must_equal 10
+      end
+    end
+
+    describe "test 3" do
+      it "5 digits match" do
+        digit_match(1062503891, 62530841).must_equal 5
+      end
+    end
+
+    describe "test 4" do
+      it "0 digits match" do
+        digit_match(4567, 45678).must_equal 0
+      end
     end
   end
 
-  describe "test 2" do
-    it "10 digits match" do
-      digit_match(3862947593, 3862947593).must_equal 10
+  # if the parameter is an object, check for nil
+  describe "nil object input test" do
+    it "nil object is not an integer" do
+      is_palindrome(nil).must_equal false
     end
   end
 
-  describe "test 3" do
-    it "5 digits match" do
-      digit_match(1062503891, 62530841).must_equal 5
-    end
-  end
-
-  describe "test 4" do
-    it "0 digits match" do
-      digit_match(4567, 45678).must_equal 0
-    end
-  end
 end
